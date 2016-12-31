3066253b55c369c6ced7795ae331de32e894d57b
Vladimir Ljubicic <ljubicic.vladimir@hotmail.com>
2016-12-31 01:43:00 +0100

    speedcube test first commit

--- SpeedCubeInterviewTest/CodedUITest1.cs
+++ SpeedCubeInterviewTest/CodedUITest1.cs
@@ -1,20 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text.RegularExpressions;
-using System.Windows.Input;
-using System.Windows.Forms;
-using System.Drawing;
-using Microsoft.VisualStudio.TestTools.UITesting;
+﻿using Microsoft.VisualStudio.TestTools.UITesting;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Microsoft.VisualStudio.TestTools.UITest.Extension;
-using Keyboard = Microsoft.VisualStudio.TestTools.UITesting.Keyboard;
-
+using Microsoft.VisualStudio.TestTools.UITesting.HtmlControls;
+using System;
+using System.Diagnostics;
 
 namespace SpeedCubeInterviewTest
-{
-    /// <summary>
-    /// Summary description for CodedUITest1
-    /// </summary>
+{   
     [CodedUITest]
     public class CodedUITest1
     {
@@ -22,11 +13,87 @@ namespace SpeedCubeInterviewTest
         {
         }
 
+        static Process proc = null;
+
+        [ClassInitialize]
+        public static void initializeTest(TestContext context)  // launching browser before the test
+        {
+            Playback.Initialize();
+            var bw = BrowserWindow.Launch();
+            proc = bw.Process;
+            bw.CloseOnPlaybackCleanup = false;
+        }
+
+        [ClassCleanup]                                          // closing browser after the test is finishe
+        public static void CleanUp()
+        {
+            BrowserWindow bw = BrowserWindow.FromProcess(proc);
+            bw.Close();
+        }
+
+
         [TestMethod]
+        [DeploymentItem("data.csv")]
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.CSV", "|DataDirectory|\\data.csv", "data#csv", DataAccessMethod.Sequential)]
         public void CodedUITestMethod1()
         {
-            // To generate code for this test, select "Generate Code for Coded UI Test" from the shortcut menu and select one of the menu items.
+            BrowserWindow browser = BrowserWindow.FromProcess(proc);
+            browser.NavigateToUrl(new Uri("http://speedcube.rs"));                  // step 1 - go to speedcube website                   
+            ClickAllCubes(browser, "Sve Kocke");                                    // step 2 - Sve Kocke button selected from the home page
+            ClickAddToCart(browser, TestContext.DataRow["cubetag"].ToString());     // step 3 - Adds cube to cart
+            ClickConfirmOrder(browser, "naručujem");                                // step 4 - Confirm order button click
+            EnterText(browser, "email", TestContext.DataRow["email"].ToString());       //
+            EnterText(browser, "name", TestContext.DataRow["name"].ToString());        //
+            EnterText(browser, "address", TestContext.DataRow["address"].ToString()); // steps 5-11 populate all of
+            EnterText(browser, "postnum", TestContext.DataRow["postnum"].ToString()); // the text fields
+            EnterText(browser, "city", TestContext.DataRow["city"].ToString());        //
+            EnterText(browser, "phone", TestContext.DataRow["phone"].ToString());       //
+            EnterComment(browser, "comment", TestContext.DataRow["comment"].ToString()); //
+            ClickCheckBox(browser, "terms");                                             // step 12 - Ticks the checkbox
+
         }
+         
+        void ClickAllCubes(UITestControl parent, string innerText)
+        {
+            var link = new HtmlHyperlink(parent);
+            link.SearchProperties.Add(HtmlHyperlink.PropertyNames.InnerText, innerText);
+            Mouse.Click(link);
+        }
+
+        void ClickAddToCart(UITestControl parent, string tagInstance)
+        {
+            var button = new HtmlImage(parent);
+            button.SearchProperties.Add(HtmlImage.PropertyNames.TagInstance, tagInstance);
+            Mouse.Click(button);
+        }
+
+        void ClickConfirmOrder(UITestControl parent, string alt)
+        {
+            var button = new HtmlImage(parent);
+            button.SearchProperties.Add(HtmlImage.PropertyNames.Alt, alt);
+            Mouse.Click(button);
+        }
+
+        void EnterText(UITestControl parent, string name, string value)
+        {
+            var edit = new HtmlEdit(parent);
+            edit.SearchProperties.Add(HtmlEdit.PropertyNames.Name, name);
+            edit.Text = value;
+        }
+
+        void EnterComment(UITestControl parent, string name, string value)
+        {
+            var edit = new HtmlTextArea(parent);
+            edit.SearchProperties.Add(HtmlTextArea.PropertyNames.Name, name);
+            edit.Text = value;
+        }
+
+        void ClickCheckBox(UITestControl parent, string id)
+        {
+            var checkBox = new HtmlCheckBox(parent);
+            checkBox.SearchProperties.Add(HtmlCheckBox.PropertyNames.Id, id);
+            Mouse.Click(checkBox);
+        }        
 
         #region Additional test attributes
 
